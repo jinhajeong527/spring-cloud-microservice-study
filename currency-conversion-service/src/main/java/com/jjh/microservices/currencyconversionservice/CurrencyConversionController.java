b23c16fad228d7f5f44f147b01586e5613a47e7e
jinha <jinha4ever@gmail.com>
2022-09-19 08:40:43 +0900

    RestTemplate 이용하여 CurrencyConversionService에서 CurrencyExchangeService 호출하는 REST API 호출

--- currency-conversion-service/src/main/java/com/jjh/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/jjh/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,18 +1,35 @@
 package com.jjh.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
         @PathVariable String from,
         @PathVariable String to,
         @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = 
+            new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+        CurrencyConversion.class, uriVariables); // 리스폰스 타입, uri 변수 키와 값 담은 해시맵 
+        
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, 
+                                      currencyConversion.getConversionMultiple(),
+                                      quantity.multiply(currencyConversion.getConversionMultiple()),
+                                      currencyConversion.getEnvironment());
     }
 }
