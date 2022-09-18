5efd9a503287625756234235e1f942e420007b24
jinha <jinha4ever@gmail.com>
2022-09-18 16:51:12 +0900

    currency-conversion-service , currency-exchange-service 추가

---
+++ currency-exchange-service/src/main/java/com/jjh/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,63 @@
+package com.jjh.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CurrencyExchange {
+	
+	@Id
+    private Long id;
+
+	@Column(name = "currency_from")
+	private String from; //SQL Keyword
+	@Column(name = "currency_to")
+	private String to;
+    // we would want to be really really accurate so we use BigDecimal
+	private BigDecimal conversionMultiple;
+	private String environment;
+    // JPA를 사용할 경우 인자 없는 생성자가 필요하다.
+	public CurrencyExchange() {
+	}
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+	public String getEnvironment() {
+		return environment;
+	}
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
+}
