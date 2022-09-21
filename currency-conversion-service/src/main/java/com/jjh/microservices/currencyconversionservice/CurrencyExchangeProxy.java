0d3d2eb11a734df245f35a90a9aacc559b5b996d
jinha <jinha4ever@gmail.com>
2022-09-22 07:16:14 +0900

    스프링 클라우드 로드Balancer를 이용한 로드 발란싱 테스트

--- currency-conversion-service/src/main/java/com/jjh/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/jjh/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //Currency Exchange Service 프로젝트의 properties 파일에 spring.application.name과 똑같은 이름으로 맞춰주어야 한다.
-@FeignClient(name="currency-exchange", url="http://localhost:8000")
+//@FeignClient(name="currency-exchange", url="http://localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
