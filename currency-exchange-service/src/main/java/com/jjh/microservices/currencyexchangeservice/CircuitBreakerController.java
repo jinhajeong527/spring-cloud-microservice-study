6cffee4eeb76097ab49dc875b997e9a47982d2d1
jinha <jinha4ever@gmail.com>
2022-09-30 16:21:07 +0900

    @Retry 커스텀 재시도 설정 및 fallback method 구현

--- currency-exchange-service/src/main/java/com/jjh/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/jjh/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.jjh.microservices.currencyexchangeservice;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +14,15 @@ import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
-    
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    //@Retry // 첫번째 시도에 성공할 것이므로 재시도되지 않을 것이다. 실패 유발 코드를 추가할 것이다. 
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse") // 첫번째 시도에 성공할 것이므로 재시도되지 않을 것이다. 실패 유발 코드를 추가할 것이다. 
     public String sampleApi() {
+        logger.info("Sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return forEntity.getBody();
+    }
+    public String hardcodedResponse(Exception ex) {
+        return "fallback-response";
     }
 }
