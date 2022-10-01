95c92d0471142336701eca11e18ef529d2ad9a03
jinha <jinha4ever@gmail.com>
2022-10-01 13:05:40 +0900

    @CircuitBreaker 실습

--- currency-exchange-service/src/main/java/com/jjh/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/jjh/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 
@@ -16,7 +17,8 @@ import io.github.resilience4j.retry.annotation.Retry;
 public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse") // 첫번째 시도에 성공할 것이므로 재시도되지 않을 것이다. 실패 유발 코드를 추가할 것이다. 
+    //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse") // 첫번째 시도에 성공할 것이므로 재시도되지 않을 것이다. 실패 유발 코드를 추가할 것이다. 
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
     public String sampleApi() {
         logger.info("Sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
