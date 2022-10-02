2250a48a7838b60110b6ff08b901665d66cb0361
jinha <jinha4ever@gmail.com>
2022-10-02 09:13:06 +0900

    Zipkin에 연결하기 위한 의존성 및 샘플러 설정 추가.

--- currency-exchange-service/src/main/java/com/jjh/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/jjh/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.jjh.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-
+    Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+    
     @Autowired
     CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -21,6 +24,7 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
         @PathVariable String from, 
         @PathVariable String to) {
+            logger.info("retrieveExchangevalue called with {} to {}", from, to);
             CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
             if(currencyExchange == null) {
                 throw new RuntimeException("Unable to Find data for " +from + " to "+ to);
